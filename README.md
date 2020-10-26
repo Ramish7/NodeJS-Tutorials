295aec4273e6e72e43880bc00bb0ea54d4df7552
Ramish SAEED <ramish.saeed@amadeus.com>
2020-10-26 12:11:46 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+An effort to learn NodeJS. That's it. That's the repository!
---
+++ node-express/index.js
@@ -0,0 +1,34 @@
+const express = require('express'),
+     http = require('http'),
+	 morgan = require('morgan'),
+	 bodyParser = require('body-parser');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+const dishRouter = require('./routes/dishRouter');
+app.use('/dishes', dishRouter);
+
+const promoRouter = require('./routes/promoRouter');
+app.use('/promotions', promoRouter);
+
+const leaderRouter = require('./routes/leaderRouter');
+app.use('/leadership', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
